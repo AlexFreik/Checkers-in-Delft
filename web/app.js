b583cb5aea6c1ee043d81d5b03c139e7e9214036
karol-202 <karoljurski1@gmail.com>
2021-12-22 16:45:00 +0100

    Reorganize server creation. Add websockets

--- web/app.js
+++ web/app.js
@@ -1,35 +1,60 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const http = require('http');
+const websocket = require('ws');
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 
-var app = express();
+function createApp(port) {
+  const app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+  app.set('port', port);
 
-app.use('/', indexRouter);
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+  app.use('/', indexRouter);
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    next(createError(404));
+  });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-module.exports = app;
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+
+  return app;
+}
+
+function createWebsocketServer(httpServer) {
+  const wss = new websocket.Server({ httpServer });
+  wss.on("connection", function (ws) {
+    ws.on("message", function incoming(message) {
+
+    });
+  });
+}
+
+function createServer(port) {
+  const app = createApp(port);
+  const server = http.createServer(app);
+  createWebsocketServer(server);
+
+  return server;
+}
+
+module.exports = createServer;
