6add626ad9fbb4c253c6198d33b14bdf0d11fe2b
karol-202 <karoljurski1@gmail.com>
2021-12-22 16:57:28 +0100

    Prettify

--- web/app.js
+++ web/app.js
@@ -1,60 +1,58 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const http = require('http');
-const websocket = require('ws');
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const http = require('http')
+const websocket = require('ws')
 
-const indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index')
 
 function createApp(port) {
-  const app = express();
+    const app = express()
 
-  app.set('port', port);
+    app.set('port', port)
 
-  app.use(logger('dev'));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, 'public')));
+    app.use(logger('dev'))
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: false }))
+    app.use(cookieParser())
+    app.use(express.static(path.join(__dirname, 'public')))
 
-  app.use('/', indexRouter);
+    app.use('/', indexRouter)
 
-  // catch 404 and forward to error handler
-  app.use(function(req, res, next) {
-    next(createError(404));
-  });
+    // catch 404 and forward to error handler
+    app.use(function (req, res, next) {
+        next(createError(404))
+    })
 
-  // error handler
-  app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // error handler
+    app.use(function (err, req, res, next) {
+        // set locals, only providing error in development
+        res.locals.message = err.message
+        res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-  });
+        // render the error page
+        res.status(err.status || 500)
+        res.render('error')
+    })
 
-  return app;
+    return app
 }
 
 function createWebsocketServer(httpServer) {
-  const wss = new websocket.Server({ httpServer });
-  wss.on("connection", function (ws) {
-    ws.on("message", function incoming(message) {
-
-    });
-  });
+    const wss = new websocket.Server({ httpServer })
+    wss.on('connection', function (ws) {
+        ws.on('message', function incoming(message) {})
+    })
 }
 
 function createServer(port) {
-  const app = createApp(port);
-  const server = http.createServer(app);
-  createWebsocketServer(server);
+    const app = createApp(port)
+    const server = http.createServer(app)
+    createWebsocketServer(server)
 
-  return server;
+    return server
 }
 
-module.exports = createServer;
+module.exports = createServer
